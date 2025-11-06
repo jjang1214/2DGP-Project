f9fa637530e34f18b50b1a7710665f201e20490a
J1214 <horse121464@gmail.com>
2025-11-06 21:53:53 +0900

    Create play_mode.py

---
+++ code/play_mode.py
@@ -0,0 +1,41 @@
+from pico2d import *
+
+import game_framework
+import game_world
+
+from character1 import character1 as char1
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            char1.handle_event(event)
+
+
+def init():
+    global char1
+
+    char1 = char1()
+    game_world.add_object(char1, 1)
+    game_world.add_collision_pair('char1:??', char1, None)
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
